46eba19f676dd660c6bf5b269e8e6e9534ce7155
Daniel Rojo <danielrojo@gmail.com>
2021-09-23 14:18:05 +0200

    apod component and service added

--- src/app/services/apod.service.ts
+++ src/app/services/apod.service.ts
@@ -8,6 +8,6 @@ export class ApodService {
   constructor(public http: HttpClient) { }
 
   getApod(): Observable<any> {
-    return this.http.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2021-06-25')
+    return this.http.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
   }
 }
